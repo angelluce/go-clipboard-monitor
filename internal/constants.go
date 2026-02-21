2eae5bf80fcd1a4397f45ce95c4da829b13e455d
angellucero <angelluce244@gmail.com>
2026-02-20 21:49:15 -0500

    feature: colors, strings constants

---
+++ internal/constants.go
@@ -0,0 +1,14 @@
+package internal
+
+const (
+	ColorReset   = "\033[0m"
+	ColorGreen   = "\033[32m"
+	ColorYellow  = "\033[33m"
+	ColorBlue    = "\033[34m"
+	ColorPrimary = "\033[92m"
+)
+
+const (
+	BoxTop    = "╭──────────────────────────────────────────────────────────╮"
+	BoxBottom = "╰──────────────────────────────────────────────────────────╯"
+)
